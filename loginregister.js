b60037af6b0968742101758bd0ebda379987cb67
avryabadd <ajabad42@tntech.edu>
2025-03-24 21:23:10 -0500

    remove discord validation
    added highlights to invalidated things

--- loginregister.js
+++ loginregister.js
@@ -31,55 +31,70 @@ document.querySelector("#toggleConfirmPassword_Register").addEventListener("clic
     
 document.querySelector("#studentRegister").addEventListener("click", () => {
     const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    const regDiscord = /^(?:[a-z0-9](?:[a-z0-9_]{1,31})|.{2,32}#[0-9]{4})$/
     const regPhone = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
     let strEmail = $('#txtEmail').val()
     let strPassword = $('#txtPassword').val().trim()
     let strFirstName = $('#txtFirstName').val().trim()
     let strLastName = $('#txtLastName').val().trim()
     let strConfirmPassword = $('#txtConfirmPassword').val().trim()
-    let phone = $('#phone').val().trim();
+    let phone = $('#telPhone').val().trim();
     let strDiscord = $('#inputDiscord').val().trim()
     let blnError = false
     let strMessage = ''
     if(!regEmail.test(strEmail)){
-        blnError = true
-        strMessage += '<p  class="mb-0 mt-0">Email must be a proper email</p>'
+        blnError = true;
+        $('#txtEmail').addClass('is-invalid');
+        strMessage += '<p class="mb-0 mt-0">Email must be a proper email</p>';
+    }  else {
+        $('#txtEmail').removeClass('is-invalid');
     }
+
     if(strPassword.length < 1){
         blnError = true
+        $('#txtPassword').addClass('is-invalid');
         strMessage += '<p class="mb-0 mt-0">Password Cannot Be Blank</p>'
     }
     else if((strPassword.length < 8) || (strPassword.length > 64) )
     {
         blnError = true
+        $('#txtPassword').addClass('is-invalid');
         strMessage += '<p class="mb-0 mt-0">Password must be between 8 and 64 characters </p>'
     }
     else
     {
         if (!(strConfirmPassword == strPassword)) {
             blnError = true
+            $('#txtConfirmPassword').addClass('is-invalid');
             strMessage += '<p class="mb-0 mt-0">Password and comfirm password must be equal</p>'
+        } else {
+            $('#txtConfirmPassword').removeClass('is-invalid');
+
         }
+        $('#txtPassword').removeClass('is-invalid');
     }
+
     if(strFirstName.length < 1){
         blnError = true
+        $('#txtFirstName').addClass('is-invalid');
         strMessage += '<p class="mb-0 mt-0">First name Cannot Be Blank</p>'
+    } else {
+        $('#txtFirstName').removeClass('is-invalid');
     }
+
     if(strLastName.length < 1){
         blnError = true
+        $('#txtLastName').addClass('is-invalid');
         strMessage += '<p class="mb-0 mt-0">Last name Cannot Be Blank</p>'
+    } else {
+        $('#txtLastName').removeClass('is-invalid');
     }
-    if (!(strDiscord === '')) {
-        if (!regDiscord.test(strDiscord)) {
-            blnError = true
-            strMessage += '<p  class="mb-0 mt-0">Discord info must be a proper Discord</p>'
-        }
-    }
-    
+  
     if(!regPhone.test(phone)){
+        $('#telPhone').addClass('is-invalid');
         blnError = true;
         strMessage += '<p class="mb-0 mt-0">Phone number must be in format 123-456-7890</p>';
+    } else {
+        $('#telPhone').removeClass('is-invalid');
     }
 
     if(blnError){
